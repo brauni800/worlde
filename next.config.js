1d846aeb5163ab97eeec6e2fa4a97d8fb4b74eca
Braulio Betancourt <braulio.1796@gmail.com>
2022-12-11 16:51:21 -0600

    add: svgr library and next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     appDir: true
+  },
+  webpack (config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack']
+    })
+    return config
   }
 }
 
